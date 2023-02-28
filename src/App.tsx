6d8936e1efd5065662f8e7d79ceff617d9bbb537
Thiagolino8 <thiagolinog8@gmail.com>
2023-02-28 10:58:37 -0300

    Fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { useComputed, effect, useSignal } from '@preact/signals-react'
 import { Counter } from './components/Counter'
